b161fc46b876bf2f9751fa9a9d9b3e382e4b41c3
Julia Martelo <julia.martelo@aluno.senai.br>
2025-08-19 10:56:40 -0300

    :tada:Criando o projeto GaleriaOnline

---
+++ DbContextImagem/GaleriaOnlineDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using GaleriaOnline.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaleriaOnline.WebApi.DbContextImagem;
+
+public partial class GaleriaOnlineDbContext : DbContext
+{
+    public GaleriaOnlineDbContext()
+    {
+    }
+
+    public GaleriaOnlineDbContext(DbContextOptions<GaleriaOnlineDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Imagen> Imagens { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-VINIDR3;Database=GaleriaOnlineDB;user ID= sa; Pwd=Senai@134;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Imagen>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Imagens__3214EC078B3FACC8");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
