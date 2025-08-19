2dfe45945d1da7eb27b847b1bed049882a9853c7
Julia Martelo <julia.martelo@aluno.senai.br>
2025-08-19 16:45:22 -0300

    Subindo os metodos do projeto

--- DbContextImagem/GaleriaOnlineDbContext.cs
+++ DbContextImagem/GaleriaOnlineDbContext.cs
@@ -16,7 +16,7 @@ public partial class GaleriaOnlineDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Imagen> Imagens { get; set; }
+    public virtual DbSet<Imagem> Imagens { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -24,7 +24,7 @@ public partial class GaleriaOnlineDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Imagen>(entity =>
+        modelBuilder.Entity<Imagem>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Imagens__3214EC078B3FACC8");
         });
--- obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b161fc46b876bf2f9751fa9a9d9b3e382e4b41c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
